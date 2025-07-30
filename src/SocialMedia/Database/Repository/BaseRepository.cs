70c1d8176bf2097707e1d90272ec0c31e8ecf6fe
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2025-07-30 15:00:52 +0300

    Add Repository Pattern

---
+++ src/SocialMedia/Database/Repository/BaseRepository.cs
@@ -0,0 +1,150 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using SocialMedia.Data.Repository.Interfaces;
+using SocialMedia.Database;
+
+namespace SocialMedia.Data.Repository
+{
+    public class BaseRepository<TType, TId> : IRepository<TType, TId>
+        where TType : class
+    {
+
+        private readonly SocialMediaDbContext dbContext;
+        private readonly DbSet<TType> dbSet;
+
+        public BaseRepository(SocialMediaDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            this.dbSet = this.dbContext.Set<TType>();
+        }
+
+        public TType GetById(TId id)
+        {
+            TType entity = this.dbSet
+                .Find(id);
+
+            return entity;
+        }
+
+        public async Task<TType> GetByIdAsync(TId id)
+        {
+            TType entity = await this.dbSet
+                .FindAsync(id);
+
+            return entity;
+        }
+        public async Task<TType> GetByIdAsync(params TId[] id)
+        {
+            //Temp patch... Fix ASAP
+            TType entity = await this.dbSet
+                .FindAsync(id[0], id[1]);
+
+            return entity;
+        }
+
+        public IEnumerable<TType> GetAll() => this.dbSet.ToArray();
+
+        public async Task<IEnumerable<TType>> GetAllAsync() =>
+            await this.dbSet.ToArrayAsync();
+
+        public TType FirstOrDefault(Func<TType, bool> predicate)
+        {
+            TType entity = this.dbSet
+                .FirstOrDefault(predicate);
+
+            return entity;
+        }
+
+        public async Task<TType> FirstOrDefaultAsync(Expression<Func<TType, bool>> predicate)
+        {
+            TType entity = await this.dbSet
+                .FirstOrDefaultAsync(predicate);
+
+            return entity;
+        }
+
+        public IQueryable<TType> GetAllAttached() => this.dbSet.AsQueryable();
+
+        public void Add(TType entity)
+        {
+            this.dbSet.Add(entity);
+            this.dbContext.SaveChanges();
+        }
+
+        public async Task AddAsync(TType entity)
+        {
+            await this.dbSet.AddAsync(entity);
+            await this.dbContext.SaveChangesAsync();
+        }
+
+        public void AddRange(TType[] items)
+        {
+            this.dbSet.AddRange(items);
+            this.dbContext.SaveChanges();
+        }
+
+        public async Task AddRangeAsync(TType[] items)
+        {
+            await this.dbSet.AddRangeAsync(items);
+            await this.dbContext.SaveChangesAsync();
+        }
+
+        public bool Delete(TType entity)
+        {
+            this.dbSet.Remove(entity);
+            this.dbContext.SaveChanges();
+
+            return true;
+        }
+
+        public async Task<bool> DeleteAsync(TType entity)
+        {
+            this.dbSet.Remove(entity);
+            await this.dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public bool Update(TType entity)
+        {
+            try
+            {
+                this.dbSet.Attach(entity);
+                this.dbContext.Entry(entity).State = EntityState.Modified;
+                this.dbContext.SaveChanges();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> UpdateAsync(TType entity)
+        {
+            try
+            {
+                this.dbSet.Attach(entity);
+                this.dbContext.Entry(entity).State = EntityState.Modified;
+                await this.dbContext.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public void SaveChanges()
+        {
+            dbContext.SaveChanges();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/SocialMedia/Database/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+
+namespace SocialMedia.Data.Repository.Interfaces
+{
+    public interface IRepository<TType, TId>
+    {
+        TType GetById(TId id);
+        Task<TType> GetByIdAsync(TId id);
+        Task<TType> GetByIdAsync(params TId[] id);
+
+        IEnumerable<TType> GetAll();
+        Task<IEnumerable<TType>> GetAllAsync();
+
+        TType FirstOrDefault(Func<TType, bool> predicate);
+        Task<TType> FirstOrDefaultAsync(Expression<Func<TType, bool>> predicate);
+
+        IQueryable<TType> GetAllAttached();
+
+        void Add(TType entity);
+        Task AddAsync(TType entity);
+
+        void AddRange(TType[] entities);
+        Task AddRangeAsync(TType[] entities);
+
+        bool Delete(TType entity);
+
+        bool Update(TType entity);
+        Task<bool> UpdateAsync(TType entity);
+
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
